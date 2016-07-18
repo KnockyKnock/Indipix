e25c1ea65de54eff0127183eb4cfcc6df1740b51
Oleg Baranok <oleg.baranok@gmail.com>
2016-07-18 16:22:38 +0000

    Добавлен jQuery анимация для телефона

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 	
-	$('.menu li a').each(function () {
+	$(".menu li a").each(function () {
 		if (this.href == location.href) {
 			this.className = "active";
 		}
+	});
+	
+	$(".main .image-phone").click(function () {
+		$(this).toggleClass("small");
+		// $(".main-content, .form").toggleClass("wide");
 	});
 	
 	
